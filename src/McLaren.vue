b010fdd585b1a905648537174ff2da67a03e353e
axelsole <axelsole@iesmontsia.org>
2022-04-27 20:27:38 +0200

    primer commit

---
+++ src/McLaren.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container text-center  mt-5 mb-5">
+    <h1 class="mt-5 fw-bolder text-success "> Kimi's Database </h1>
+     <div class="table-responsive my-5">
+      
+      <!-- The table component -->
+       <Table :fields='fields' :kimiquotes ="kimiquotes"></Table>
+     </div>
+ 
+</div>
+</template>
+
+<script>
+// Importing the table component
+import Table from './components/Table.vue'
+
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Table
+  },
+  data() {
+    return {
+      kimiquotes: [],
+    };
+  },
+  created() {
+          axios.get('https://kimiquotes.herokuapp.com/quotes/2006').then((response) => {
+              //console.log(response);
+              this.kimiquotes = response.data;
+    });
+  },
+   setup(){
+     
+     const fields = [
+      'id','quote','year'
+    ]
+    return{fields}
+  },
+
+}
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,76 @@
+<template>
+
+  <div class="searchBar">
+    <!-- Filter Search -->
+      <div class="input-group mb-5">
+        <input type="search" class="form-control" v-model='searchQuery' placeholder="Kimi's quote" aria-label="Recipient's username" aria-describedby="button-addon2">
+      </div>
+  </div>
+
+<table id="tableComponent" class="table table-bordered table-striped">
+<caption>The Iceman</caption>
+  <thead>
+    <tr>
+      <!-- loop through each value of the fields to get the table header -->
+      <th  v-for="field in fields" :key='field' @click="sortTable(field)" > 
+        {{field}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+       </th>
+    </tr>
+  </thead>
+  <tbody>
+      <!-- Loop through the list get the each student data -->
+      <tr v-for="item in filteredList" :key='item'>
+      <td v-for="field in fields" :key='field'>{{item[field]}}</td>
+    </tr>
+  </tbody>
+</table> 
+</template>
+<script>
+import {computed,ref} from "vue";
+// Importing  the lodash library 
+import { sortBy} from 'lodash';
+
+export default {
+  name: 'TableComponent',
+  props:{
+      kimiquotes: Array,
+      fields: Array
+  },
+  
+  setup(props) {
+    let sort = ref(false);
+    let updatedList =  ref([])
+    let searchQuery = ref("");
+    
+        // a function to sort the table
+    const sortTable = (col) => {
+      sort.value = true
+       // Use of _.sortBy() method
+      updatedList.value = sortBy(props.kimiquotes,col)
+      }
+
+    const sortedList = computed(() => {
+      if (sort.value) {
+         return updatedList.value
+      }
+      else{
+         return props.kimiquotes;
+      }
+      });
+
+
+      // Filter Search
+      const filteredList = computed(() => {
+          return sortedList.value.filter((product) => {
+            return (
+              product.quote.toLowerCase().indexOf(searchQuery.value.toLowerCase()) != -1
+            );
+          });
+});   
+      
+
+  return {sortedList, sortTable,searchQuery,filteredList}
+  }
+ 
+}
+</script>
